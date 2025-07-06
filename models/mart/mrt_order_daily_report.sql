1180499b8be13953a1f251fde1dc221d1b09e463
unknown <matteo.dijoux@outlook.com>
2025-07-06 18:44:38 +0200

    delete left join for the job

--- models/mart/mrt_order_daily_report.sql
+++ models/mart/mrt_order_daily_report.sql
@@ -2,7 +2,7 @@ SELECT DATE_TRUNC(order_created_at, DAY) AS reporting_date,
 
     COUNT(DISTINCT order_id) AS total_orders,
     AVG(total_distinct_items) AS average_total_distinct_items,
-    SUM(sum_feedbackgit_score) / SUM(total_feedbacks) AS average_feedback_score,
+    SUM(sum_feedback_score) / SUM(total_feedbacks) AS average_feedback_score,
     AVG(total_order_amount) AS average_total_order_amount
 FROM {{ ref('int_sales_database__order') }} AS orders
 
