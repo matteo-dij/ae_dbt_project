2ac419bae2d6a27078289cfeec6200fd6997baa7
unknown <matteo.dijoux@outlook.com>
2025-07-06 21:15:24 +0200

    delete product column the other was for feedback

--- models/staging/sales_database/stg_sales_database__product.sql
+++ models/staging/sales_database/stg_sales_database__product.sql
@@ -4,7 +4,7 @@ select product_id,
  cast(product_description_lenght as int) as product_description_lenght,
  cast(product_photos_qty as int) as product_photos_qty,
  cast(product_weight_g as int) as product_weight_g,
- cast(product_length_cm as int) as product_length_cm,
+ --cast(product_length_cm as int) as product_length_cm,
  cast(product_height_cm as int) as product_height_cm,
  cast(product_width_cm as int) as product_width_cm
 from {{ source('sales_database', 'product') }}
